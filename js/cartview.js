92bf23194517edf814293ba15138195d040401cb
robertcinca <robert.cinca.14@ucl.ac.uk>
2017-03-29 10:38:17 +0800

    cart view with js

--- js/cartview.js
+++ js/cartview.js
@@ -1,12 +1,13 @@
 //Function that extracts cart data from JSON format and converts it into a table display
 function getShoppingCart() {
     var x = getParameterByName('username'); //get username
-    var accountInfo = getAccounts(x); //get JSON from function
+    var cartInfo = getAccounts(x); //get JSON from function
+    var totalAmount = 0; //Calculate total price of all purchased books
     
     //Extracting Table headings
     var col = [];
-    for (var i = 0; i < accountInfo.length; i++) {
-        for (var key in accountInfo[i]) {
+    for (var i = 0; i < cartInfo.length; i++) {
+        for (var key in cartInfo[i]) {
             if (col.indexOf(key) === -1) {
                 col.push(key);
             }
@@ -26,19 +27,35 @@ function getShoppingCart() {
     }
     
     // Add JSON to table
-    for (var i = 0; i < accountInfo.length; i++) {
+    for (var i = 0; i < cartInfo.length; i++) {
         
         tr = table.insertRow(-1);
         
         // for (var j = 0; j < 1; j++) {
         //     var tabCell = tr.insertCell(-1);
-        //     tabCell.innerHTML = (accountInfo[i][col[j]]).link("accountdetail.html" + "?accNo=" + (accountInfo[i][col[j]]));
+        //     tabCell.innerHTML = (cartInfo[i][col[j]]).link("accountdetail.html" + "?accNo=" + (cartInfo[i][col[j]]));
         // }
         for (var j = 0; j < col.length; j++) {
             var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = accountInfo[i][col[j]];
+            tabCell.innerHTML = cartInfo[i][col[j]];
+            if (cartInfo[i][col[col.length - 1]]) {
+                totalAmount += parseInt(cartInfo[i][col[col.length -1]]);
+                alert(parseInt(cartInfo[i][col[col.length -1]]));
+            }
         }
     }
+
+    //Create last row
+    var footer = table.createTFoot();
+    tr = footer.insertRow(-1);
+    for (var j = 0; j < col.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        if (j == col.length - 2)
+            tabCell.innerHTML = "Total Price (HKD):";
+        if (j == col.length - 1)
+            tabCell.innerHTML = totalAmount;
+    }
+
     
     // Add table with JSON data to a container
     var divContainer = document.getElementById("showData");
@@ -57,7 +74,7 @@ function getAccounts(user) {
             "Author":"J. K. Rowling",
             "Quantity": 1,
             "Unit Point": 20,
-            "Unit Price": 130.00,
+            "Unit Price": 130,
             "Total Price": 20
          },
          {
@@ -65,8 +82,8 @@ function getAccounts(user) {
             "Author":"Jane Austen",
             "Quantity": 4,
             "Unit Point": 10,
-            "Unit Price": 140.00,
-            "Total Price": 560.00
+            "Unit Price": 140,
+            "Total Price": 560
          },
          {
             "Book Name":"Harry Potter and the Philosopher's Stone",
@@ -81,8 +98,8 @@ function getAccounts(user) {
             "Author":"Jane Austen",
             "Quantity": 4,
             "Unit Point": 10,
-            "Unit Price": 140.00,
-            "Total Price": 560.00
+            "Unit Price": 140,
+            "Total Price": 560
          },
          {
             "Book Name":"Harry Potter and the Philosopher's Stone",
@@ -97,8 +114,8 @@ function getAccounts(user) {
             "Author":"Jane Austen",
             "Quantity": 4,
             "Unit Point": 10,
-            "Unit Price": 140.00,
-            "Total Price": 560.00
+            "Unit Price": 140,
+            "Total Price": 560
          }      
         ];
         from=Math.floor(Math.random()*6);
